aa9ccaabe459e96c9991ab3a2052fc911476dc9b
MEHUL AGRAWAL <agrawalmehul33@gmail.com>
2022-04-07 00:51:01 +0530

    final

--- SPI/Master.c
+++ SPI/Master.c
@@ -7,74 +7,41 @@
 
 #include <avr/io.h>
 #define F_CPU 8000000UL
-#define BAUD_RATE ((F_CPU/(4800*16UL))-1)
 #include <util/delay.h>
 
 void SPI_MasterInit(void);
 void SPI_MasterTransmit(char cData);
 char SPI_MasterReceive();
 
-void USART_Init(void);
-void USART_Transmit(char data);
-char USART_Receive(void);
-
-
-
+uint8_t i = 0x00;
 
 int main(void)
 {
 	SPI_MasterInit();
-	//USART_Init();
-	
-	SPI_MasterTransmit(0x07);
-	_delay_ms(100);
-	PORTF = SPI_MasterReceive();
-	_delay_ms(100);
-
     while (1) 
     {
-		//char a  = USART_Receive();
-		
-		//USART_Transmit(b);
+		SPI_MasterTransmit(i++);
+		PORTF = SPI_MasterReceive();
+		_delay_ms(1000);
     }
 }
 
 void SPI_MasterInit(void)
 {
-	/* Set MOSI and SCK output, all others input */
 	DDRB = (1<<DDB2)|(1<<DDB1)|(1<<DDB0);
-	/* Enable SPI, Master, set clock rate fck/16 */
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
 void SPI_MasterTransmit(char cData)
 {
-	 //Start transmission 
 	SPDR = cData;
-	 //Wait for transmission complete 
 	while(!(SPSR & (1<<SPIF)))
 	;
 }
 
 char SPI_MasterReceive(void)
 {
-	SPDR = 0x07;
 	while(!(SPSR&(1<<SPIF)));
 	return SPDR;
-}
-
-void USART_Init(){
-	UBRR0H = BAUD_RATE>>8;
-	UBRR0L = BAUD_RATE;
-	UCSR0B = 1<<RXEN0 | 1<<TXEN0;
-	UCSR0C = 0x06;
-}
-void USART_Transmit(char data){
-	while(!(UCSR0A&(1<<UDRE0)));
-	SPDR = data;
-}
-char USART_Receive(){
-	while(!(UCSR0A & (1<<RXC0)));
-	return UDR0;
 }
 
 
--- SPI/Slave.c
+++ SPI/Slave.c
@@ -7,47 +7,28 @@
 
 #include <avr/io.h>
 #define F_CPU 8000000UL
-#define BAUD_RATE ((F_CPU/(4800*16UL))-1)
 #include <util/delay.h>
 
 void SPI_SlaveInit(void);
 char SPI_SlaveReceive(void);
 void SPI_SlaveTransmit(char data);
 
-void USART_Init(void);
-char USART_Receive(void);
-void USART_Transmit(char cdata);
-
-
+uint8_t i = 0x00;
 
 int main(void)
 {
 	SPI_SlaveInit();
-	//USART_Init();
-	//DDRA = 0xFF;
-	//PORTA = 0x00;
-	PORTD = SPI_SlaveReceive();
-	_delay_ms(100);
-	SPI_SlaveTransmit(0x03);
-	_delay_ms(100);
     while (1) 
     {
-		//char a = SPI_SlaveReceive();
-		//USART_Transmit(a);
-		//char b = USART_Receive();
-		//_delay_ms(100);
-		//PORTD = SPI_SlaveReceive();
-		//_delay_ms(100);
-		//SPI_SlaveTransmit(0x03);
-		//_delay_ms(100);
+		PORTA = SPI_SlaveReceive();
+		SPI_SlaveTransmit(i++);
+		_delay_ms(1000);
     }
 }
 
 void SPI_SlaveInit(void)
 {
-	/* Set MISO output, all others input */
 	DDRB = (1<<DDB3);
-	/* Enable SPI */
 	SPCR = (1<<SPE);
 }
 
@@ -59,24 +40,7 @@ void SPI_SlaveTransmit(char data)
 
 char SPI_SlaveReceive(void)
 {
-	/* Wait for reception complete */
 	while(!(SPSR & (1<<SPIF)));
-	/* Return data register */
 	return SPDR;
-}
-
-void USART_Init(void){
-	UCSR0B = 1<<TXEN0 | 1<<RXEN0;
-	UCSR0C = 0x06; 
-	UBRR0H = BAUD_RATE>>8;
-	UBRR0L = BAUD_RATE;
-}
-void USART_Transmit(char cdata){
-	while(!(UCSR0A & 1<<UDRE0));
-	UDR0 = cdata;
-}
-char USART_Receive(void){
-	while(!(UCSR0A&(1<<RXC0)));
-	return UDR0;	
 }
 
